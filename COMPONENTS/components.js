3f2db97848dc2d86ce0a6c7632ddd284bfbdeffd
Pavan Yadav <pavan.yadav.sde@gmail.com>
2026-05-12 08:05:27 +0530

    Initial commit

---
+++ COMPONENTS/components.js
@@ -0,0 +1,21 @@
+function renderHeader(basePath) {
+    document.write(`
+    <nav class="navbar">
+        <a href="${basePath}index.html" class="brand">
+            <img src="${basePath}IMAGE/PPOST__1_-removebg-preview.png" alt="PPOST Logo">
+        </a>
+        <div class="nav-links">
+            <a href="${basePath}index.html" class="nav-link">Home</a>
+            <a href="${basePath}contact.html" class="nav-link">Contact</a>
+        </div>
+    </nav>
+    `);
+}
+
+function renderFooter() {
+    document.write(`
+    <footer>
+        &copy; 2026 PPOST. All rights reserved. <br>Transforming India from consumers to creators.
+    </footer>
+    `);
+}
